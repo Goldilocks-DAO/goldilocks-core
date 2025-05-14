13328ad86482cdcb82fbc3b8edbdb6f1fe8985c8
0xgeeb <0xgeeb@gmail.com>
2025-05-14 11:10:51 -0400

    add goldilend token tests

---
+++ test/unit/GPRG.t.sol
@@ -0,0 +1,30 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import { BaseUnitTest } from "../base/BaseUnitTest.t.sol";
+import { GPRG } from "../../src/core/goldilend/GPRG.sol";
+import { Goldilend } from "../../src/core/goldilend/Goldilend.sol";
+
+contract UnitGPRGTest is BaseUnitTest {
+
+  function testGPRGName() public {
+    assertEq(gprg.name(), "Goldilend Porridge");
+  }
+
+  function testGPRGSymbol() public {
+    assertEq(gprg.symbol(), "GPRG");
+  }
+
+  function testMintGprgFailGoldilend() public {
+    vm.prank(address(0x69));
+    vm.expectRevert(abi.encodeWithSelector(GPRG.NotGoldilend.selector));
+    gprg.mintGPRG(address(0x69), 69);
+  }
+
+  function testBurnGprgFailGoldilend() public {
+    vm.prank(address(0x69));
+    vm.expectRevert(abi.encodeWithSelector(GPRG.NotGoldilend.selector));
+    gprg.burnGPRG(address(0x69), 69);
+  }
+
+}
