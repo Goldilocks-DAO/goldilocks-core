4b55fb9fc6b4830a7870de60fc1d936742789bc5
0xgeeb <0xgeeb@gmail.com>
2025-08-28 15:18:18 -0400

    add fuzz tests

--- test/fuzz/RebaseGoldilend.t.sol
+++ test/fuzz/RebaseGoldilend.t.sol
@@ -2,12 +2,15 @@
 pragma solidity ^0.8.22;
 
 import { BaseFuzzTest } from "../base/BaseFuzzTest.t.sol";
+import { IERC721 } from "../../lib/openzeppelin-contracts/contracts/token/ERC721/IERC721.sol";
+import { INFT } from "../../src/mock/INFT.sol";
 import { IRebaseGoldilend } from "../../src/interfaces/IRebaseGoldilend.sol";
 import { RebaseGoldilend } from "../../src/core/goldilend/RebaseGoldilend.sol";
 
 contract FuzzRebaseGoldilendTest is BaseFuzzTest {
 
     function testFuzzDeposit(uint256 depositAmount) public {
+        vm.assume(depositAmount > 0 && depositAmount < 1e40);
         deal(address(honey), address(this), depositAmount);
         honey.approve(address(rebaseproxy), depositAmount);
         RebaseGoldilend(address(rebaseproxy)).deposit(depositAmount);
@@ -33,62 +36,175 @@ contract FuzzRebaseGoldilendTest is BaseFuzzTest {
         assertEq(RebaseGoldilend(address(rebaseproxy)).poolSize(), depositAmount - withdrawAmount);
     }
 
-    function testFuzzRebaseBorrow(uint256 durationAmount) public dealUserBeras {
-        // vm.assume(durationAmount > goldilend.minDuration() && durationAmount < goldilend.maxDuration());
-        // goldilend.borrow(1e18, durationAmount, address(bondbear), 1);
-        // Goldilend.Loan memory userLoan = goldilend.lookupLoan(address(this), 1);
-
-        // assertEq(userLoan.collateralNFTs[0], address(bondbear));
-        // assertEq(userLoan.collateralNFTIds[0], 1);
-        // assertEq(userLoan.duration, durationAmount);
-        // assertEq(userLoan.endDate, block.timestamp + durationAmount);
-        // assertEq(userLoan.loanId, 1);
-        // assertEq(userLoan.liquidated, false);
+    function testFuzzBorrow(uint256 borrowAmount, uint256 duration) public {
+        vm.assume(borrowAmount > 0 && borrowAmount < 1e17);
+        vm.assume(duration >= 1 days && duration <= 365 days);
+        
+        INFT(address(bandbear)).mint(address(this));
+        IERC721(bandbear).setApprovalForAll(address(rebaseproxy), true);
+        
+        deal(address(honey), address(this), 1e30);
+        honey.approve(address(rebaseproxy), 1e30);
+        RebaseGoldilend(address(rebaseproxy)).deposit(1e30);
+        
+        uint256 initialBalance = honey.balanceOf(address(this));
+        RebaseGoldilend(address(rebaseproxy)).borrow(borrowAmount, duration, address(bandbear), 1);
+        
+        assertEq(honey.balanceOf(address(this)), initialBalance + borrowAmount);
+        assertEq(RebaseGoldilend(address(rebaseproxy)).outstandingDebt(), borrowAmount);
     }
 
-      function testFuzzRepayiBGT(uint256 durationAmount) public dealUserBeras { // had dealUseriBGT ehere
-        // vm.assume(durationAmount > goldilend.minDuration() && durationAmount < goldilend.maxDuration());
-        // goldilend.borrow(1e18, durationAmount, address(bondbear), 1);
-        // Goldilend.Loan memory userLoanBefore = goldilend.lookupLoan(address(this), 1);
-        // uint256 interestLoanRatio = FixedPointMathLib.divWad(userLoanBefore.interest, userLoanBefore.borrowedAmount);
-        // uint256 interest = FixedPointMathLib.mulWadUp(1e18+userLoanBefore.interest, interestLoanRatio);
-        // goldilend.repay(1e18+userLoanBefore.interest, 1);
-        // Goldilend.Loan memory userLoan = goldilend.lookupLoan(address(this), 1);
-
-        // assertEq(IERC721(address(bondbear)).balanceOf(address(goldilend)), 0);
-        // assertEq(IERC721(address(bondbear)).balanceOf(address(this)), 1);
-        // assertEq(userLoan.collateralNFTs[0], address(bondbear));
-        // assertEq(userLoan.collateralNFTIds[0], 1);
-        // assertEq(userLoan.borrowedAmount, 0);
-        // assertLe(userLoan.interest, 2);
-        // assertEq(userLoan.duration, durationAmount);
-        // assertEq(userLoan.endDate, block.timestamp + durationAmount);
-        // assertEq(userLoan.loanId, 1);
-        // assertEq(userLoan.liquidated, false);
-        // assertEq(goldilend.outstandingDebt(), 0);
-        // assertEq(goldilend.poolSize(), 1000e18 + (interest * 950 / 1000));
+    function testFuzzRepay(uint256 borrowAmount, uint256 repayAmount) public {
+        vm.assume(borrowAmount > 0 && borrowAmount < 1e17);
+        vm.assume(repayAmount > 0 && repayAmount <= borrowAmount);
+        
+        INFT(address(bandbear)).mint(address(this));
+        IERC721(bandbear).setApprovalForAll(address(rebaseproxy), true);
+        
+        deal(address(honey), address(this), 1e30);
+        honey.approve(address(rebaseproxy), 1e30);
+        RebaseGoldilend(address(rebaseproxy)).deposit(1e30);
+        
+        RebaseGoldilend(address(rebaseproxy)).borrow(borrowAmount, 30 days, address(bandbear), 1);
+        uint256 initialBalance = honey.balanceOf(address(this));
+        honey.approve(address(rebaseproxy), repayAmount);
+        RebaseGoldilend(address(rebaseproxy)).repay(repayAmount, 1);
+        
+        assertEq(honey.balanceOf(address(this)), initialBalance - repayAmount);
+        assertEq(RebaseGoldilend(address(rebaseproxy)).outstandingDebt(), borrowAmount - repayAmount);
     }
 
-  function testFuzzLiquidate(uint256 time, uint256 durationAmount) public dealUserBeras { // had dealUseriBGT ehere
-        // vm.assume(time < type(uint256).max / 2);
-        // vm.assume(durationAmount > goldilend.minDuration() && durationAmount < goldilend.maxDuration());
-        // goldilend.borrow(1e18, durationAmount, address(bondbear), 1);
-        // vm.warp(time);
-        // Goldilend.Loan memory userLoan = goldilend.lookupLoan(address(this), 1);    
-        // if(time > userLoan.endDate + 86401) {
-        //   goldilend.liquidate(address(this), 1);
-        //   assertEq(userLoan.collateralNFTs[0], address(bondbear));
-        //   assertEq(userLoan.collateralNFTIds[0], 1);
-        //   assertEq(userLoan.duration, durationAmount);
-        //   assertEq(userLoan.endDate, durationAmount + 1);
-        //   assertEq(userLoan.loanId, 1);
-        //   assertEq(IERC721(address(bondbear)).balanceOf(address(goldilend)), 0);
-        //   assertEq(IERC721(address(bondbear)).balanceOf(address(this)), 1);
-        // }
-        // else {
-        //   vm.expectRevert(abi.encodeWithSelector(IGoldilend.Unliquidatable.selector));
-        //   goldilend.liquidate(address(this), 1);
-        // }
+    function testFuzzLiquidate(uint256 borrowAmount) public {
+        vm.assume(borrowAmount > 0 && borrowAmount < 1e17);
+        
+        INFT(address(bandbear)).mint(address(this));
+        IERC721(bandbear).setApprovalForAll(address(rebaseproxy), true);
+        
+        deal(address(honey), address(this), 1e30);
+        honey.approve(address(rebaseproxy), 1e30);
+        RebaseGoldilend(address(rebaseproxy)).deposit(1e30);
+        
+        RebaseGoldilend(address(rebaseproxy)).borrow(borrowAmount, 1 days, address(bandbear), 1);
+        vm.warp(block.timestamp + 2 days);
+        
+        uint256 initialOutstandingDebt = RebaseGoldilend(address(rebaseproxy)).outstandingDebt();
+        RebaseGoldilend(address(rebaseproxy)).liquidate(address(this), 1);
+        
+        assertEq(RebaseGoldilend(address(rebaseproxy)).outstandingDebt(), initialOutstandingDebt - borrowAmount);
+    }
+
+    function testFuzzMultipleDeposits(uint256[] calldata amounts) public {
+        vm.assume(amounts.length > 0 && amounts.length <= 5);
+        
+        for (uint256 i = 0; i < amounts.length; i++) {
+            vm.assume(amounts[i] > 0 && amounts[i] < 1e25);
+            deal(address(honey), address(this), amounts[i]);
+            honey.approve(address(rebaseproxy), amounts[i]);
+            RebaseGoldilend(address(rebaseproxy)).deposit(amounts[i]);
+        }
+        
+        uint256 totalDeposited = 0;
+        for (uint256 i = 0; i < amounts.length; i++) {
+            totalDeposited += amounts[i];
+        }
+        
+        assertEq(RebaseGoldilend(address(rebaseproxy)).poolSize(), totalDeposited);
+    }
+
+    function testFuzzMultipleBorrows(uint256 borrowAmount1, uint256 borrowAmount2) public {
+        vm.assume(borrowAmount1 > 0 && borrowAmount1 < 1e16);
+        vm.assume(borrowAmount2 > 0 && borrowAmount2 < 1e16);
+        
+        deal(address(honey), address(this), 1e30);
+        honey.approve(address(rebaseproxy), 1e30);
+        RebaseGoldilend(address(rebaseproxy)).deposit(1e30);
+        
+        INFT(address(bandbear)).mint(address(this));
+        INFT(address(bandbear)).mint(address(this));
+        IERC721(bandbear).setApprovalForAll(address(rebaseproxy), true);
+        
+        RebaseGoldilend(address(rebaseproxy)).borrow(borrowAmount1, 30 days, address(bandbear), 1);
+        RebaseGoldilend(address(rebaseproxy)).borrow(borrowAmount2, 30 days, address(bandbear), 2);
+        
+        assertEq(RebaseGoldilend(address(rebaseproxy)).outstandingDebt(), borrowAmount1 + borrowAmount2);
+    }
+
+    function testFuzzRepayPartialAndFull(uint256 borrowAmount, uint256 partialRepay) public {
+        vm.assume(borrowAmount > partialRepay && partialRepay > 0);
+        vm.assume(borrowAmount < 1e17 && partialRepay < 1e17);
+        
+        INFT(address(bandbear)).mint(address(this));
+        IERC721(bandbear).setApprovalForAll(address(rebaseproxy), true);
+        
+        deal(address(honey), address(this), 1e30);
+        honey.approve(address(rebaseproxy), 1e30);
+        RebaseGoldilend(address(rebaseproxy)).deposit(1e30);
+        
+        RebaseGoldilend(address(rebaseproxy)).borrow(borrowAmount, 30 days, address(bandbear), 1);
+        
+        honey.approve(address(rebaseproxy), partialRepay);
+        RebaseGoldilend(address(rebaseproxy)).repay(partialRepay, 1);
+        assertEq(RebaseGoldilend(address(rebaseproxy)).outstandingDebt(), borrowAmount - partialRepay);
+        
+        honey.approve(address(rebaseproxy), borrowAmount - partialRepay);
+        RebaseGoldilend(address(rebaseproxy)).repay(borrowAmount - partialRepay, 1);
+        assertEq(RebaseGoldilend(address(rebaseproxy)).outstandingDebt(), 0);
+    }
+
+    function testFuzzBorrowWithDifferentCollateral(uint256 borrowAmount, uint256 duration) public {
+        vm.assume(borrowAmount > 0 && borrowAmount < 1e17);
+        vm.assume(duration >= 1 days && duration <= 365 days);
+        
+        deal(address(honey), address(this), 1e30);
+        honey.approve(address(rebaseproxy), 1e30);
+        RebaseGoldilend(address(rebaseproxy)).deposit(1e30);
+        
+        INFT(address(bandbear)).mint(address(this));
+        IERC721(bandbear).setApprovalForAll(address(rebaseproxy), true);
+        
+        uint256 initialBalance = honey.balanceOf(address(this));
+        RebaseGoldilend(address(rebaseproxy)).borrow(borrowAmount, duration, address(bandbear), 1);
+        
+        assertEq(honey.balanceOf(address(this)), initialBalance + borrowAmount);
+        assertEq(RebaseGoldilend(address(rebaseproxy)).outstandingDebt(), borrowAmount);
+    }
+
+    function testFuzzMaxUtilization(uint256 depositAmount, uint256 borrowAmount) public {
+        vm.assume(depositAmount > 1e20 && depositAmount < 1e25);
+        vm.assume(borrowAmount > 0 && borrowAmount < 1e17);
+        vm.assume(borrowAmount < depositAmount / 10);
+        
+        INFT(address(bandbear)).mint(address(this));
+        IERC721(bandbear).setApprovalForAll(address(rebaseproxy), true);
+        
+        deal(address(honey), address(this), depositAmount);
+        honey.approve(address(rebaseproxy), depositAmount);
+        RebaseGoldilend(address(rebaseproxy)).deposit(depositAmount);
+        
+        RebaseGoldilend(address(rebaseproxy)).borrow(borrowAmount, 30 days, address(bandbear), 1);
+        
+        uint256 utilization = (RebaseGoldilend(address(rebaseproxy)).outstandingDebt() * 100) / RebaseGoldilend(address(rebaseproxy)).poolSize();
+        assertLe(utilization, 90);
+    }
+
+
+    function testFuzzLoanGracePeriod(uint256 borrowAmount) public {
+        vm.assume(borrowAmount > 0 && borrowAmount < 1e17);
+        
+        INFT(address(bandbear)).mint(address(this));
+        IERC721(bandbear).setApprovalForAll(address(rebaseproxy), true);
+        
+        deal(address(honey), address(this), 1e30);
+        honey.approve(address(rebaseproxy), 1e30);
+        RebaseGoldilend(address(rebaseproxy)).deposit(1e30);
+        
+        RebaseGoldilend(address(rebaseproxy)).borrow(borrowAmount, 1 days, address(bandbear), 1);
+        
+        vm.warp(block.timestamp + 1 days + 12 hours);
+        honey.approve(address(rebaseproxy), borrowAmount);
+        RebaseGoldilend(address(rebaseproxy)).repay(borrowAmount, 1);
+        
+        assertEq(RebaseGoldilend(address(rebaseproxy)).outstandingDebt(), 0);
     }
 
 }
