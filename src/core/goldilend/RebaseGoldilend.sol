70da0c5d9c002355dfea2d0abd96d9826402d0e8
0xgeeb <0xgeeb@gmail.com>
2025-08-28 14:52:29 -0400

    quick fix for incorrect local variable

--- src/core/goldilend/RebaseGoldilend.sol
+++ src/core/goldilend/RebaseGoldilend.sol
@@ -222,7 +222,7 @@ contract RebaseGoldilend is Initializable, OwnableUpgradeable, UUPSUpgradeable, 
         Loan memory userLoan = loans[msg.sender][userLoanId];
         if(repayAmount > userLoan.borrowedAmount) repayAmount = userLoan.borrowedAmount;
         if(block.timestamp > userLoan.endDate + LOAN_GRACE_PERIOD) revert LoanExpired();
-        if(repayAmount > _outstandingDebt) repayAmount = _outstandingDebt;
+        if(repayAmount > outstandingDebt) repayAmount = outstandingDebt;
         outstandingDebt -= repayAmount;
         SafeTransferLib.safeTransferFrom(debtAsset, msg.sender, address(this), repayAmount);
         if(userLoan.borrowedAmount - repayAmount == 0) {
