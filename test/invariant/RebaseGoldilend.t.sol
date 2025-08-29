7d7c815cde96576bf29c636227e01e917549c5f6
0xgeeb <0xgeeb@gmail.com>
2025-08-29 13:46:43 -0400

    add more rebasegoldilend invariant tests

--- test/invariant/RebaseGoldilend.t.sol
+++ test/invariant/RebaseGoldilend.t.sol
@@ -3,19 +3,30 @@ pragma solidity ^0.8.20;
 
 import { BaseInvariantTest } from "../base/BaseInvariantTest.t.sol";
 import { RebaseGoldilendHandler } from "../invariant/handlers/RebaseGoldilendHandler.t.sol";
+import { IERC721 } from "../../lib/openzeppelin-contracts/contracts/token/ERC721/IERC721.sol";
+import { RebaseGoldilend } from "../../src/core/goldilend/RebaseGoldilend.sol";
 
 contract InvariantRebaseGoldilendTest is BaseInvariantTest {
 
   function setUp() public override {
     deployProtocol();
 
-    rebasegoldilendHandler = new RebaseGoldilendHandler(address(rebaseproxy), glhoney, honey);
-    bytes4[] memory rebasegoldilendSelectors = new bytes4[](5);
+    deal(address(honey), address(this), 1_000_000e18);
+    honey.approve(address(rebaseproxy), 1_000_000e18);
+    RebaseGoldilend(address(rebaseproxy)).deposit(500_000e18);
+
+    rebasegoldilendHandler = new RebaseGoldilendHandler(address(rebaseproxy), glhoney, honey, bandbear);
+    bytes4[] memory rebasegoldilendSelectors = new bytes4[](9);
     rebasegoldilendSelectors[0] = rebasegoldilendHandler.deposit.selector;
     rebasegoldilendSelectors[1] = rebasegoldilendHandler.withdraw.selector;
-    rebasegoldilendSelectors[2] = rebasegoldilendHandler.approve.selector;
-    rebasegoldilendSelectors[3] = rebasegoldilendHandler.transfer.selector;
-    rebasegoldilendSelectors[4] = rebasegoldilendHandler.transferFrom.selector;
+    rebasegoldilendSelectors[2] = rebasegoldilendHandler.borrow.selector;
+    rebasegoldilendSelectors[3] = rebasegoldilendHandler.renew.selector;
+    rebasegoldilendSelectors[4] = rebasegoldilendHandler.repay.selector;
+    rebasegoldilendSelectors[5] = rebasegoldilendHandler.liquidate.selector;
+    rebasegoldilendSelectors[6] = rebasegoldilendHandler.approve.selector;
+    rebasegoldilendSelectors[7] = rebasegoldilendHandler.transfer.selector;
+    rebasegoldilendSelectors[8] = rebasegoldilendHandler.transferFrom.selector;
+    
     targetSelector(FuzzSelector({
       addr: address(rebasegoldilendHandler),
       selectors: rebasegoldilendSelectors
@@ -31,7 +42,7 @@ contract InvariantRebaseGoldilendTest is BaseInvariantTest {
     assertTrue(address(rebasegoldilendHandler) != address(0));
     assertTrue(address(rebasegoldilendHandler.rebasegoldilend()) != address(0));
   }
-
+ 
   function invariant_glhoney_eq_deposited() public {
     uint256 sumOfMinted = rebasegoldilendHandler.reduceActors(
       0,
@@ -43,8 +54,242 @@ contract InvariantRebaseGoldilendTest is BaseInvariantTest {
     );
   }
 
-  function invariant_glhoney_lte_poolsize() public {
-    rebasegoldilendHandler.forEachActor(this.assertMintedGlhoneyLtePoolSize);
+  function invariant_glhoney_supply_consistency() public {
+    assertGe(glhoney.totalSupply(), 0, "glHONEY supply should be >= 0");
+    if (RebaseGoldilend(address(rebaseproxy)).poolSize() > 0) {
+      assertGt(glhoney.totalSupply(), 0, "glHONEY supply should be positive when pool size is positive");
+    }
+  }
+
+  function invariant_glhoney_accounting() public {
+    uint256 totalMinted = rebasegoldilendHandler.ghost_depositSum() - rebasegoldilendHandler.ghost_withdrawSum();
+    uint256 actualSupply = glhoney.totalSupply();
+    uint256 initialDeposit = 500_000e18;
+    uint256 expectedTotalMinted = totalMinted + initialDeposit;
+    uint256 tolerance = expectedTotalMinted / 1000; // 0.1% tolerance
+    
+    assertGe(actualSupply, expectedTotalMinted - tolerance, "glHONEY supply should be close to net deposits");
+    assertLe(actualSupply, expectedTotalMinted + tolerance, "glHONEY supply should be close to net deposits");
+  }
+
+  function invariant_individual_glhoney_balances() public {
+    rebasegoldilendHandler.forEachActor(this.assertIndividualGlhoneyBalanceValid);
+  }
+
+  function invariant_pool_size_consistency() public {
+    assertGe(RebaseGoldilend(address(rebaseproxy)).poolSize(), 0);
+    assertLe(RebaseGoldilend(address(rebaseproxy)).outstandingDebt(), RebaseGoldilend(address(rebaseproxy)).poolSize());
+    
+    if (RebaseGoldilend(address(rebaseproxy)).poolSize() > 0) {
+      assertLe(
+        RebaseGoldilend(address(rebaseproxy)).outstandingDebt(), 
+        RebaseGoldilend(address(rebaseproxy)).poolSize() * RebaseGoldilend(address(rebaseproxy)).maxUtilization() / 100
+      );
+    }
+  }
+
+  function invariant_debt_accounting() public {
+    assertGe(
+      rebasegoldilendHandler.getTotalBorrowed(),
+      rebasegoldilendHandler.getTotalRepaid()
+    );
+    
+    assertGe(
+      rebasegoldilend.outstandingDebt(),
+      rebasegoldilendHandler.getTotalBorrowed() - rebasegoldilendHandler.getTotalRepaid()
+    );
+    
+    assertLe(
+      RebaseGoldilend(address(rebaseproxy)).outstandingDebt(),
+      RebaseGoldilend(address(rebaseproxy)).poolSize()
+    );
+  }
+
+  function invariant_collateral_tracking() public {
+    assertGe(
+      rebasegoldilendHandler.ghost_collateralDeposited(),
+      rebasegoldilendHandler.ghost_collateralReturned() + rebasegoldilendHandler.ghost_collateralLiquidated()
+    );
+    
+    uint256 activeLoans = rebasegoldilendHandler.getTotalActiveLoans();
+    uint256 collateralInProtocol = rebasegoldilendHandler.ghost_collateralDeposited() - 
+                                   rebasegoldilendHandler.ghost_collateralReturned() - 
+                                   rebasegoldilendHandler.ghost_collateralLiquidated();
+    assertEq(activeLoans, collateralInProtocol);
+    
+    rebasegoldilendHandler.forEachActor(this.assertActiveLoansHaveCollateral);
+  }
+
+  function invariant_loan_state_consistency() public {
+    rebasegoldilendHandler.forEachActor(this.assertActiveLoansValid);
+  }
+
+  function invariant_interest_calculation() public {
+    if (rebasegoldilendHandler.getTotalBorrowed() > 0) {
+      assertGt(rebasegoldilendHandler.ghost_interestSum(), 0);
+    }
+  }
+
+  function invariant_liquidation_safety() public {
+    rebasegoldilendHandler.forEachActor(this.assertLiquidatedLoansInactive);
+  }
+
+  function invariant_repayment_safety() public {
+    rebasegoldilendHandler.forEachActor(this.assertRepaidLoansInactive);
+  }
+
+  function invariant_borrowing_limits() public {
+    rebasegoldilendHandler.forEachActor(this.assertLoanAmountsWithinLimits);
+  }
+
+  function invariant_nft_ownership() public {
+    rebasegoldilendHandler.forEachActor(this.assertNFTOwnershipValid);
+  }
+
+  function invariant_loan_duration_limits() public {
+    rebasegoldilendHandler.forEachActor(this.assertLoanDurationValid);
+  }
+
+  function invariant_loan_interest_consistency() public {
+    rebasegoldilendHandler.forEachActor(this.assertLoanInterestValid);
+  }
+
+  function invariant_protocol_parameters() public {
+    if (RebaseGoldilend(address(rebaseproxy)).parametersInitialized()) {
+      assertGt(RebaseGoldilend(address(rebaseproxy)).maxDuration(), RebaseGoldilend(address(rebaseproxy)).minDuration(), "Max duration should be > min duration");
+      assertLe(RebaseGoldilend(address(rebaseproxy)).maxUtilization(), 100, "Max utilization should be <= 100%");
+      assertGt(RebaseGoldilend(address(rebaseproxy)).maxUtilization(), 0, "Max utilization should be > 0");
+      assertGt(RebaseGoldilend(address(rebaseproxy)).protocolInterestRate(), 0, "Protocol interest rate should be > 0");
+      assertGt(RebaseGoldilend(address(rebaseproxy)).slope(), 0, "Slope should be > 0");
+    }
+  }
+
+  function invariant_protocol_addresses() public {
+    assertTrue(RebaseGoldilend(address(rebaseproxy)).multisig() != address(0), "Multisig should be set");
+    assertTrue(RebaseGoldilend(address(rebaseproxy)).timelock() != address(0), "Timelock should be set");
+    assertTrue(RebaseGoldilend(address(rebaseproxy)).debtAsset() != address(0), "Debt asset should be set");
+    assertTrue(RebaseGoldilend(address(rebaseproxy)).glDebtAsset() != address(0), "GL debt asset should be set");
+  }
+
+  function invariant_nft_fair_values() public {
+    if (RebaseGoldilend(address(rebaseproxy)).berasInitialized()) {
+      uint256 bandbearValue = RebaseGoldilend(address(rebaseproxy)).nftFairValues(address(bandbear));
+      assertGt(bandbearValue, 0, "BandBear NFT should have a fair value set");
+    }
+  }
+
+  function assertActiveLoansValid(address actor) external returns (address[] memory) {
+    for (uint256 i = 1; i <= rebasegoldilendHandler.userLoanCount(actor); i++) {
+      if (rebasegoldilendHandler.activeLoans(actor, i)) {
+        RebaseGoldilend.Loan memory loan = RebaseGoldilend(address(rebaseproxy)).getUserLoan(actor, i);
+        assertFalse(loan.repaid, "Active loan should not be repaid");
+        assertFalse(loan.liquidated, "Active loan should not be liquidated");
+        assertGt(loan.borrowedAmount, 0, "Active loan should have borrowed amount");
+      }
+    }
+    return new address[](0);
+  }
+
+  function assertLiquidatedLoansInactive(address actor) external returns (address[] memory) {
+    for (uint256 i = 1; i <= rebasegoldilendHandler.userLoanCount(actor); i++) {
+      RebaseGoldilend.Loan memory loan = RebaseGoldilend(address(rebaseproxy)).getUserLoan(actor, i);
+      if (loan.liquidated) {
+        assertFalse(rebasegoldilendHandler.activeLoans(actor, i), "Liquidated loan should not be active");
+      }
+    }
+    return new address[](0);
+  }
+
+  function assertRepaidLoansInactive(address actor) external returns (address[] memory) {
+    for (uint256 i = 1; i <= rebasegoldilendHandler.userLoanCount(actor); i++) {
+      RebaseGoldilend.Loan memory loan = RebaseGoldilend(address(rebaseproxy)).getUserLoan(actor, i);
+      if (loan.repaid) {
+        assertFalse(rebasegoldilendHandler.activeLoans(actor, i), "Repaid loan should not be active");
+      }
+    }
+    return new address[](0);
+  }
+
+  function assertLoanAmountsWithinLimits(address actor) external returns (address[] memory) {
+    for (uint256 i = 1; i <= rebasegoldilendHandler.userLoanCount(actor); i++) {
+      RebaseGoldilend.Loan memory loan = RebaseGoldilend(address(rebaseproxy)).getUserLoan(actor, i);
+      if (loan.borrowedAmount > 0) {
+        assertLe(loan.borrowedAmount, RebaseGoldilend(address(rebaseproxy)).poolSize() / 10, "Loan amount exceeds limit");
+      }
+    }
+    return new address[](0);
+  }
+
+  function assertNFTOwnershipValid(address actor) external returns (address[] memory) {
+    for (uint256 i = 1; i <= rebasegoldilendHandler.userLoanCount(actor); i++) {
+      RebaseGoldilend.Loan memory loan = RebaseGoldilend(address(rebaseproxy)).getUserLoan(actor, i);
+      if (loan.borrowedAmount > 0 && !loan.repaid && !loan.liquidated) {
+        assertEq(
+          IERC721(loan.collateralNFT).ownerOf(loan.collateralNFTId),
+          address(rebaseproxy),
+          "Active loan NFT should be owned by protocol"
+        );
+      } else if (loan.repaid) {
+        assertEq(
+          IERC721(loan.collateralNFT).ownerOf(loan.collateralNFTId),
+          actor,
+          "Repaid loan NFT should be returned to user"
+        );
+      } else if (loan.liquidated) {
+        assertEq(
+          IERC721(loan.collateralNFT).ownerOf(loan.collateralNFTId),
+          RebaseGoldilend(address(rebaseproxy)).multisig(),
+          "Liquidated loan NFT should be owned by multisig"
+        );
+      }
+    }
+    return new address[](0);
+  }
+
+  function assertIndividualGlhoneyBalanceValid(address actor) external returns (address[] memory) {
+    uint256 balance = glhoney.balanceOf(actor);
+    uint256 totalSupply = glhoney.totalSupply();
+    
+    assertLe(balance, totalSupply, "Individual glHONEY balance should not exceed total supply");
+    if (totalSupply > 0) {
+      assertLe(balance, totalSupply, "Individual balance should not exceed total supply");
+    }
+    
+    return new address[](0);
+  }
+
+  function assertLoanDurationValid(address actor) external returns (address[] memory) {
+    for (uint256 i = 1; i <= rebasegoldilendHandler.userLoanCount(actor); i++) {
+      RebaseGoldilend.Loan memory loan = RebaseGoldilend(address(rebaseproxy)).getUserLoan(actor, i);
+      if (loan.borrowedAmount > 0 && !loan.repaid && !loan.liquidated) {
+        assertGe(loan.duration, RebaseGoldilend(address(rebaseproxy)).minDuration(), "Loan duration should be >= min duration");
+        assertLe(loan.duration, RebaseGoldilend(address(rebaseproxy)).maxDuration(), "Loan duration should be <= max duration");
+        assertGt(loan.endDate, block.timestamp, "Active loan end date should be in the future");
+      }
+    }
+    return new address[](0);
+  }
+
+  function assertLoanInterestValid(address actor) external returns (address[] memory) {
+    for (uint256 i = 1; i <= rebasegoldilendHandler.userLoanCount(actor); i++) {
+      RebaseGoldilend.Loan memory loan = rebasegoldilend.getUserLoan(actor, i);
+      if (loan.borrowedAmount > 0 && !loan.repaid && !loan.liquidated) {
+        assertGt(loan.interest, 0, "Active loan should have positive interest");
+        assertLe(loan.interest, loan.borrowedAmount * 10, "Interest should not be unreasonably high");
+      }
+    }
+    return new address[](0);
+  }
+
+  function assertActiveLoansHaveCollateral(address actor) external returns (address[] memory) {
+    for (uint256 i = 1; i <= rebasegoldilendHandler.userLoanCount(actor); i++) {
+      RebaseGoldilend.Loan memory loan = RebaseGoldilend(address(rebaseproxy)).getUserLoan(actor, i);
+      if (loan.borrowedAmount > 0 && !loan.repaid && !loan.liquidated) {
+        assertTrue(loan.collateralNFT != address(0), "Active loan should have collateral NFT");
+        assertGt(loan.collateralNFTId, 0, "Active loan should have valid collateral NFT ID");
+      }
+    }
+    return new address[](0);
   }
 
 }
--- test/invariant/handlers/RebaseGoldilendHandler.t.sol
+++ test/invariant/handlers/RebaseGoldilendHandler.t.sol
@@ -6,34 +6,64 @@ import { SafeTransferLib } from "../../../lib/solady/src/utils/SafeTransferLib.s
 import { RebaseGoldilend } from "../../../src/core/goldilend/RebaseGoldilend.sol";
 import { GoldilendDebtAsset } from "../../../src/core/goldilend/GoldilendDebtAsset.sol";
 import { Honey } from "../../../src/mock/Honey.sol";
+import { BandBear } from "../../../src/mock/BandBear.sol";
+import { IERC721 } from "../../../lib/openzeppelin-contracts/contracts/token/ERC721/IERC721.sol";
 
 contract RebaseGoldilendHandler is BaseHandler {
 
   RebaseGoldilend public rebasegoldilend;
   GoldilendDebtAsset public glhoney;
   Honey public honey;
+  BandBear public bandbear;
 
   uint256 public ghost_depositSum;
   uint256 public ghost_withdrawSum;
   uint256 public ghost_zeroWithdraws;
+  uint256 public ghost_borrowSum;
+  uint256 public ghost_repaySum;
+  uint256 public ghost_liquidateSum;
+  uint256 public ghost_renewSum;
+  uint256 public ghost_interestSum;
+  uint256 public ghost_collateralDeposited;
+  uint256 public ghost_collateralReturned;
+  uint256 public ghost_collateralLiquidated;
 
-  constructor(address _rebaseproxy, GoldilendDebtAsset _glhoney, Honey _honey) {
+  mapping(address => uint256) public userLoanCount;
+  mapping(address => mapping(uint256 => bool)) public activeLoans;
+  mapping(address => uint256) public userTotalBorrowed;
+  mapping(address => uint256) public userTotalRepaid;
+  mapping(address => uint256[]) public userNFTs;
+  mapping(address => uint256) public userNFTCount;
+
+  constructor(address _rebaseproxy, GoldilendDebtAsset _glhoney, Honey _honey, BandBear _bandbear) {
     rebasegoldilend = RebaseGoldilend(_rebaseproxy);
     glhoney = _glhoney;
     honey = _honey;
+    bandbear = _bandbear;
     deal(address(honey), address(this), 100_000e18);
+  }
+
+  function mintNFTForActor(address actor) external {
+    if (userNFTCount[actor] == 0) {
+      bandbear.mint(actor);
+      userNFTs[actor].push(userNFTCount[actor] + 1);
+      userNFTCount[actor]++;
+    }
   }
 
   function deposit(uint256 amount) public createActor countCall("deposit") {
     amount = bound(amount, 0, honey.balanceOf(address(this)));
+    amount = bound(amount, 0, 1_000_000e18);
 
     sendHoney(currentActor, amount);
     vm.startPrank(currentActor);
     honey.approve(address(rebasegoldilend), amount);
-    rebasegoldilend.deposit(amount);
+    try RebaseGoldilend(address(rebasegoldilend)).deposit(amount) {
+      ghost_depositSum += amount;
+    } catch {
+      // Deposit failed, continue
+    }
     vm.stopPrank();
-
-    ghost_depositSum += amount;
   }
 
   function withdraw(uint256 actorSeed, uint256 amount) public useActor(actorSeed) countCall("withdraw") {
@@ -41,10 +71,127 @@ contract RebaseGoldilendHandler is BaseHandler {
     if(amount == 0) ghost_zeroWithdraws++;
 
     vm.startPrank(currentActor);
-    rebasegoldilend.withdraw(amount);
+    try RebaseGoldilend(address(rebasegoldilend)).withdraw(amount) {
+      ghost_withdrawSum += amount;
+    } catch {
+      // Withdraw failed, continue
+    }
     vm.stopPrank();
+  }
 
-    ghost_withdrawSum += amount;
+  function borrow(
+    uint256 actorSeed,
+    uint256 borrowAmount,
+    uint256 duration,
+    uint256 nftId
+  ) public useActor(actorSeed) countCall("borrow") {
+    if (!rebasegoldilend.borrowingActive() || !rebasegoldilend.berasInitialized()) return;
+    if (userNFTCount[currentActor] == 0 || nftId >= userNFTCount[currentActor]) {
+      bandbear.mint(currentActor);
+      userNFTs[currentActor].push(userNFTCount[currentActor] + 1);
+      userNFTCount[currentActor]++;
+      nftId = userNFTCount[currentActor] - 1;
+    }
+    uint256 actualTokenId = userNFTs[currentActor][nftId];
+    uint256 maxBorrow = RebaseGoldilend(address(rebasegoldilend)).poolSize() / 10;
+    uint256 nftValue = RebaseGoldilend(address(rebasegoldilend)).nftFairValues(address(bandbear));
+    borrowAmount = bound(borrowAmount, 0, maxBorrow);
+    borrowAmount = bound(borrowAmount, 0, nftValue);
+    duration = bound(duration, RebaseGoldilend(address(rebasegoldilend)).minDuration(), RebaseGoldilend(address(rebasegoldilend)).maxDuration());
+
+    if (RebaseGoldilend(address(rebasegoldilend)).outstandingDebt() + borrowAmount > RebaseGoldilend(address(rebasegoldilend)).poolSize() * RebaseGoldilend(address(rebasegoldilend)).maxUtilization() / 100) return;
+    try RebaseGoldilend(address(rebasegoldilend)).borrow(borrowAmount, duration, address(bandbear), actualTokenId) {
+      userLoanCount[currentActor]++;
+      uint256 loanId = userLoanCount[currentActor];
+      activeLoans[currentActor][loanId] = true;
+      userTotalBorrowed[currentActor] += borrowAmount;
+      ghost_borrowSum += borrowAmount;
+      ghost_collateralDeposited++;
+      uint256 interest = RebaseGoldilend(address(rebasegoldilend)).calculateInterest(borrowAmount, duration, address(bandbear));
+      ghost_interestSum += interest;
+    } catch {
+      // Borrow failed, continue
+    }
+  }
+
+  function renew(
+    uint256 actorSeed,
+    uint256 loanId,
+    uint256 newDuration,
+    uint256 newBorrowAmount
+  ) public useActor(actorSeed) countCall("renew") {
+    if (!RebaseGoldilend(address(rebasegoldilend)).borrowingActive() || !RebaseGoldilend(address(rebasegoldilend)).berasInitialized()) return;
+    if (!activeLoans[currentActor][loanId]) return;
+    RebaseGoldilend.Loan memory loan = RebaseGoldilend(address(rebasegoldilend)).getUserLoan(currentActor, loanId);
+    if (loan.borrowedAmount == 0 || loan.repaid || loan.liquidated) return;
+    uint256 maxBorrow = RebaseGoldilend(address(rebasegoldilend)).poolSize() / 10;
+    newBorrowAmount = bound(newBorrowAmount, 0, maxBorrow);
+    newDuration = bound(newDuration, RebaseGoldilend(address(rebasegoldilend)).minDuration(), RebaseGoldilend(address(rebasegoldilend)).maxDuration());
+
+    if (RebaseGoldilend(address(rebasegoldilend)).outstandingDebt() + newBorrowAmount > RebaseGoldilend(address(rebasegoldilend)).poolSize() * RebaseGoldilend(address(rebasegoldilend)).maxUtilization() / 100) return;
+    try RebaseGoldilend(address(rebasegoldilend)).renew(loanId, newDuration, newBorrowAmount) {
+      userTotalBorrowed[currentActor] += newBorrowAmount;
+      ghost_renewSum += newBorrowAmount;
+      ghost_borrowSum += newBorrowAmount;
+      uint256 newInterest = RebaseGoldilend(address(rebasegoldilend)).calculateInterest(loan.borrowedAmount + newBorrowAmount, newDuration, loan.collateralNFT);
+      ghost_interestSum += newInterest;
+    } catch {
+      // Renew failed, continue
+    }
+  }
+
+  function repay(
+    uint256 actorSeed,
+    uint256 loanId,
+    uint256 repayAmount
+  ) public useActor(actorSeed) countCall("repay") {
+    if (!activeLoans[currentActor][loanId]) return;
+    RebaseGoldilend.Loan memory loan = RebaseGoldilend(address(rebasegoldilend)).getUserLoan(currentActor, loanId);
+    if (loan.borrowedAmount == 0 || loan.repaid || loan.liquidated) return;
+    uint256 actorHoney = honey.balanceOf(currentActor);
+    if (actorHoney == 0) {
+      sendHoney(currentActor, loan.borrowedAmount);
+      actorHoney = loan.borrowedAmount;
+    }
+    repayAmount = bound(repayAmount, 0, loan.borrowedAmount);
+    repayAmount = bound(repayAmount, 0, actorHoney);
+    vm.startPrank(currentActor);
+    honey.approve(address(rebasegoldilend), repayAmount);
+    try RebaseGoldilend(address(rebasegoldilend)).repay(repayAmount, loanId) {
+      userTotalRepaid[currentActor] += repayAmount;
+      ghost_repaySum += repayAmount;
+      RebaseGoldilend.Loan memory updatedLoan = RebaseGoldilend(address(rebasegoldilend)).getUserLoan(currentActor, loanId);
+      if (updatedLoan.repaid) {
+        activeLoans[currentActor][loanId] = false;
+        ghost_collateralReturned++;
+      }
+    } catch {
+      // Repay failed, continue
+    }
+    vm.stopPrank();
+  }
+
+  function liquidate(
+    uint256 actorSeed,
+    uint256 targetSeed,
+    uint256 loanId
+  ) public useActor(actorSeed) countCall("liquidate") {
+    address target = randomActor(targetSeed);
+    if (target == currentActor) return;
+    if (!activeLoans[target][loanId]) return;
+    RebaseGoldilend.Loan memory loan = RebaseGoldilend(address(rebasegoldilend)).getUserLoan(target, loanId);
+    if (loan.borrowedAmount == 0 || loan.repaid || loan.liquidated) return;
+    if (block.timestamp < loan.endDate + RebaseGoldilend(address(rebasegoldilend)).LOAN_GRACE_PERIOD()) {
+      vm.warp(loan.endDate + RebaseGoldilend(address(rebasegoldilend)).LOAN_GRACE_PERIOD() + 1);
+    }
+
+    try RebaseGoldilend(address(rebasegoldilend)).liquidate(target, loanId) {
+      activeLoans[target][loanId] = false;
+      ghost_liquidateSum += loan.borrowedAmount;
+      ghost_collateralLiquidated++;
+    } catch {
+      // Liquidation failed, continue
+    }
   }
 
   function approve(
@@ -97,6 +244,41 @@ contract RebaseGoldilendHandler is BaseHandler {
 
   function sendHoney(address actor, uint256 amount) internal {
     SafeTransferLib.safeTransfer(address(honey), actor, amount);
+  }
+
+  function getRandomNFTForActor(address actor, uint256 seed) internal view returns (uint256) {
+    if (userNFTCount[actor] == 0) return 0;
+    return userNFTs[actor][seed % userNFTCount[actor]];
+  }
+
+  // Helper functions for invariant testing
+  function getTotalActiveLoans() external view returns (uint256) {
+    uint256 total = 0;
+    for (uint256 i = 0; i < actors().length; i++) {
+      address actor = actors()[i];
+      for (uint256 j = 1; j <= userLoanCount[actor]; j++) {
+        if (activeLoans[actor][j]) {
+          total++;
+        }
+      }
+    }
+    return total;
+  }
+
+  function getTotalBorrowed() external view returns (uint256) {
+    uint256 total = 0;
+    for (uint256 i = 0; i < actors().length; i++) {
+      total += userTotalBorrowed[actors()[i]];
+    }
+    return total;
+  }
+
+  function getTotalRepaid() external view returns (uint256) {
+    uint256 total = 0;
+    for (uint256 i = 0; i < actors().length; i++) {
+      total += userTotalRepaid[actors()[i]];
+    }
+    return total;
   }
 
 }
